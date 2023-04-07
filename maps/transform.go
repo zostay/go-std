7c327c76bd23b28dc8ab0ca75bb5ecdd5224063e
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-07 16:54:14 -0500

    godoc and coverage

--- maps/transform.go
+++ maps/transform.go
@@ -1,5 +1,7 @@
 package maps
 
+// Keys will return all the keys of the input map. The keys returned are not
+// sorted.
 func Keys[K comparable, V any](in map[K]V) []K {
 	out := make([]K, 0, len(in))
 	for k := range in {
@@ -8,6 +10,8 @@ func Keys[K comparable, V any](in map[K]V) []K {
 	return out
 }
 
+// Values will return all the values of the input map. The values returend are
+// not sorted.
 func Values[K comparable, V any](in map[K]V) []V {
 	out := make([]V, 0, len(in))
 	for _, v := range in {
@@ -16,6 +20,9 @@ func Values[K comparable, V any](in map[K]V) []V {
 	return out
 }
 
+// KVs will return all the keys and values in an array. The even-indexed
+// elements will the keys and the element following is the value that belongs to
+// that key.
 func KVs[K comparable, V any](in map[K]V) []any {
 	out := make([]any, 0, len(in)*2)
 	for k, v := range in {
---
+++ maps/transform_test.go
@@ -0,0 +1,66 @@
+package maps_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zostay/go-std/maps"
+)
+
+func TestKeys(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	keys := maps.Keys(a)
+	assert.Len(t, keys, len(a))
+	for k := range a {
+		assert.Contains(t, keys, k)
+	}
+}
+
+func TestValues(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	values := maps.Values(a)
+	assert.Len(t, values, len(a))
+	for _, v := range a {
+		assert.Contains(t, values, v)
+	}
+}
+
+func TestKVs(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	kvs := maps.KVs(a)
+	assert.Len(t, kvs, len(a)*2)
+	for k, v := range a {
+		assert.Contains(t, kvs, k)
+		assert.Contains(t, kvs, v)
+
+		for i := range kvs {
+			if kvs[i] == k {
+				assert.Equal(t, v, kvs[i+1])
+			}
+		}
+	}
+
+	for i, x := range kvs {
+		if i%2 == 0 {
+			assert.IsType(t, "", x)
+		} else {
+			assert.IsType(t, 0, x)
+		}
+	}
+}
