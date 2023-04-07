9a806a96c40a3547431d4703b20bb7ff9e6180ef
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-07 15:35:12 -0500

    fix duplicated types

--- compare/minmax.go
+++ compare/minmax.go
@@ -8,7 +8,7 @@ type Able interface {
 	~uint8 | ~uint16 | ~uint32 | ~uint64 |
 		~int8 | ~int16 | ~int32 | ~int64 |
 		~float32 | ~float64 |
-		~byte | ~rune | ~uint | ~int
+		~uint | ~int
 }
 
 func Max[T Able](a, b T) T {
