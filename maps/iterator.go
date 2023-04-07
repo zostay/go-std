f24e1efe81c10021c8abff3f39a169113c051beb
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-07 16:45:12 -0500

    godoc and coverage

--- maps/iterator.go
+++ maps/iterator.go
@@ -1,11 +1,14 @@
 package maps
 
+// Iterator implements iterable.Interface for maps.
 type Iterator[K comparable, V any] struct {
 	data   map[K]V
 	index  []K
 	cursor int
 }
 
+// NewIterator returns a fresh iterator for any map. If the map is modified
+// while iteration is being performed, you may encounter unexpected results.
 func NewIterator[K comparable, V any](data map[K]V) *Iterator[K, V] {
 	index := make([]K, 0, len(data))
 	for k := range data {
@@ -14,23 +17,28 @@ func NewIterator[K comparable, V any](data map[K]V) *Iterator[K, V] {
 	return &Iterator[K, V]{data, index, -1}
 }
 
+// Len returns the number of elements that will be iterated.
 func (i *Iterator[K, V]) Len() int {
 	return len(i.data)
 }
 
+// Next moves the iterator cursor to the next item and increments Index.
 func (i *Iterator[K, V]) Next() bool {
 	i.cursor++
 	return i.cursor < len(i.data)
 }
 
+// Index returns the index of the current item being iterated over.
 func (i *Iterator[K, V]) Index() int {
 	return i.cursor
 }
 
+// ID returns the map key for the current item.
 func (i *Iterator[K, V]) ID() K {
 	return i.index[i.cursor]
 }
 
+// Val returns the map value for the current item.
 func (i *Iterator[K, V]) Val() V {
 	return i.data[i.index[i.cursor]]
 }
---
+++ maps/iterator_test.go
@@ -0,0 +1,32 @@
+package maps_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zostay/go-std/maps"
+)
+
+func TestNewIterator(t *testing.T) {
+	a := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	var expect map[string]int
+	maps.CopyInit(&expect, a)
+
+	iter := maps.NewIterator(a)
+	assert.Equal(t, len(a), iter.Len())
+	var i int
+	for iter.Next() {
+		assert.Equal(t, i, iter.Index())
+		assert.Contains(t, expect, iter.ID())
+		assert.Equal(t, expect[iter.ID()], iter.Val())
+		delete(expect, iter.ID())
+		i++
+	}
+	assert.Empty(t, expect)
+}
