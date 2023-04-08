c48d3c6c41a9c27fc0dcb595dca40273f8a77253
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-07 23:27:13 -0500

    godoc

--- slices/manipulate.go
+++ slices/manipulate.go
@@ -1,10 +1,15 @@
 package slices
 
+// Delete will remove the data at index i from the given slice. This works by
+// copying the data after it back and then shortening the slice by one element.
 func Delete[T any](slice []T, i int) []T {
 	copy(slice[i:], slice[i+1:])
 	return slice[:len(slice)-1]
 }
 
+// DeleteValue will find the first element in the slice with value s. It will
+// remove that value from the slice by copying all elements after it back one
+// index and then shortening the slice by one element.
 func DeleteValue[T comparable](slice []T, s T) []T {
 	for i, v := range slice {
 		if v == s {
@@ -14,6 +19,9 @@ func DeleteValue[T comparable](slice []T, s T) []T {
 	return slice
 }
 
+// DeleteAllValues will delete every value s in the given slice. It works the
+// same way as DeleteValue, but performs the deletion task for every element
+// matching the given s, not just the first.
 func DeleteAllValues[T comparable](slice []T, s T) []T {
 	for i := len(slice) - 1; i >= 0; i-- {
 		if slice[i] == s {
@@ -23,18 +31,29 @@ func DeleteAllValues[T comparable](slice []T, s T) []T {
 	return slice
 }
 
+// Push will add the given values to the end of the slice and return the newly
+// transformed slice. The slice must be allocated or this function will panic.
 func Push[T any](slice []T, v ...T) []T {
 	return append(slice, v...)
 }
 
+// Pop will remove one value from the end of the slice and return it and the
+// newly transform slice (shorter by one value). The slice must be allocated and
+// be non-empty or this operation will panic.
 func Pop[T any](slice []T) (T, []T) {
 	return slice[len(slice)-1], slice[:len(slice)-1]
 }
 
+// Shift will remove the first item from the slice and return it and the newly
+// transformed slice (which is one shorter). The slice must be allocated and be
+// non-empty or this function will panic.
 func Shift[T any](slice []T) (T, []T) {
 	return slice[0], slice[1:]
 }
 
+// Unshift will add the given values to the front of the slice and return the
+// newly transformed slice. The slice must be allocated or this function will
+// panic. The values will be added in the order given.
 func Unshift[T any](slice []T, vs ...T) []T {
 	var zero T
 	for range vs {
