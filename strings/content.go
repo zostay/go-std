2d901fe1d600aedfc00d47755632868fdbba1b15
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-07 23:43:19 -0500

    fix reversing, godoc and coverage

--- strings/content.go
+++ strings/content.go
@@ -1,7 +1,6 @@
 package strings
 
 import (
-	"sort"
 	"strings"
 )
 
@@ -20,17 +19,17 @@ func ContainsOnly(s, chars string) bool {
 
 type runeSlice []rune
 
-func (rs runeSlice) Less(i, j int) bool {
-	return rs[i] < rs[j]
-}
+// func (rs runeSlice) Less(i, j int) bool {
+// 	return rs[i] < rs[j]
+// }
 
 func (rs runeSlice) Swap(i, j int) {
 	rs[i], rs[j] = rs[j], rs[i]
 }
 
-func (rs runeSlice) Len() int {
-	return len(rs)
-}
+// func (rs runeSlice) Len() int {
+// 	return len(rs)
+// }
 
 // FromRange returns a string containing all the characters between two given
 // characters (inclusive).
@@ -41,14 +40,24 @@ func FromRange(a, z rune) string {
 		a, z = z, a
 	}
 
-	out := make([]rune, z-a)
-	for i := a; i < z; i++ {
+	out := make([]rune, z-a+1)
+	for i := a; i <= z; i++ {
 		out[i-a] = i
 	}
 
 	if reverse {
-		sort.Reverse(runeSlice(out))
+		return Reverse(string(out))
 	}
 
+	return string(out)
+}
+
+// Reverse returns the string reversed. That is, passing the string "Reversing
+// text." will result in this function returning ".txet gnisreveR".
+func Reverse(in string) string {
+	out := runeSlice(in)
+	for i := 0; i < len(in)-i-1; i++ {
+		out.Swap(i, len(in)-i-1)
+	}
 	return string(out)
 }
--- strings/content_test.go
+++ strings/content_test.go
@@ -1,14 +1,27 @@
-package strings
+package strings_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/zostay/go-std/strings"
 )
 
 func TestContainsOnly(t *testing.T) {
-	assert.True(t, ContainsOnly("abc", "abc"))
-	assert.True(t, ContainsOnly("a", "abc"))
-	assert.True(t, ContainsOnly("aaaaaabbbbbbccccc", "cba"))
-	assert.False(t, ContainsOnly("a b c", "abc"))
+	assert.True(t, strings.ContainsOnly("abc", "abc"))
+	assert.True(t, strings.ContainsOnly("a", "abc"))
+	assert.True(t, strings.ContainsOnly("aaaaaabbbbbbccccc", "cba"))
+	assert.False(t, strings.ContainsOnly("a b c", "abc"))
+}
+
+func TestFromRange(t *testing.T) {
+	assert.Equal(t, "abcd", strings.FromRange('a', 'd'))
+	assert.Equal(t, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", strings.FromRange('A', 'Z'))
+	assert.Equal(t, "0123456789", strings.FromRange('0', '9'))
+	assert.Equal(t, "zyxwvutsrqponmlkjihgfedcba", strings.FromRange('z', 'a'))
+}
+
+func TestReverse(t *testing.T) {
+	assert.Equal(t, ".txet gnisreveR", strings.Reverse("Reversing text."))
 }
