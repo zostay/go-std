88245072df2528c1d24456d1df86f3dc7593227a
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-04-21 20:31:24 -0500

    adding shuffle/sample/uniq functions for slices

---
+++ slices/sort.go
@@ -0,0 +1,64 @@
+package slices
+
+import "math/rand"
+
+// Shuffle uses the built-in rand.Shuffle function to shuffle a slice in place
+// without needing to define a swap function. It returns the input argument
+// after shuffling that slice in place.
+func Shuffle[T any](in []T) []T {
+	rand.Shuffle(len(in), func(i, j int) {
+		in[i], in[j] = in[j], in[i]
+	})
+	return in
+}
+
+// Sample selects count items from the list randomly and returns a new list with
+// the selected items. If count is larger than the size of the input list, this
+// works the same as Shuffle.
+func Sample[T any](count int, in []T) []T {
+	// TODO this is not very efficient
+	out := make([]T, len(in))
+	copy(out, in)
+	rand.Shuffle(len(out), func(i, j int) {
+		out[i], out[j] = out[j], out[i]
+	})
+	if len(out) > count {
+		out = out[:count]
+	}
+	return out
+}
+
+// Uniq creates a new list copied from the input, but with subsequent duplicate
+// items removed.
+func Uniq[T comparable](in []T) []T {
+	if len(in) == 0 {
+		return []T{}
+	}
+
+	out := make([]T, 1, len(in))
+	out[0] = in[0]
+
+	for i := 1; i < len(in); i++ {
+		if in[i] != out[len(out)-1] {
+			out = append(out, in[i])
+		}
+	}
+
+	return out
+}
+
+// UniqInPlace deletes subsequent duplicate items from the source list and
+// returns the now modified (possibly shorter) slice.
+func UniqInPlace[T comparable](in []T) []T {
+	if len(in) <= 1 {
+		return in
+	}
+
+	for i := len(in) - 1; i > 0; i-- {
+		if in[i] == in[i-1] {
+			in = Delete(in, i)
+		}
+	}
+
+	return in
+}
---
+++ slices/sort_test.go
@@ -0,0 +1,63 @@
+package slices_test
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zostay/go-std/generic"
+	"github.com/zostay/go-std/slices"
+)
+
+func TestShuffle(t *testing.T) {
+	in := []int{1, 2, 3, 4, 5}
+
+	slices.Shuffle(in)
+	assert.Len(t, in, 5)
+	assert.Contains(t, in, 1)
+	assert.Contains(t, in, 2)
+	assert.Contains(t, in, 3)
+	assert.Contains(t, in, 4)
+	assert.Contains(t, in, 5)
+}
+
+func TestSample(t *testing.T) {
+	ins := [][]int{
+		{},
+		{1, 2},
+		{1, 2, 3, 4, 5},
+	}
+
+	for _, in := range ins {
+		is := slices.Sample(3, in)
+
+		// all values are from the original list
+		assert.Len(t, is, generic.Min(len(in), 3))
+		for _, v := range is {
+			assert.Contains(t, in, v)
+		}
+
+		// no duplicates
+		sort.Ints(is)
+		p := -1
+		for _, v := range is {
+			assert.NotEqual(t, v, p)
+			p = v
+		}
+	}
+}
+
+func TestUniq(t *testing.T) {
+	in := []int{1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 7}
+	out := slices.Uniq(in)
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7}, out)
+	assert.Equal(t, []int{}, slices.Uniq([]int{}))
+}
+
+func TestUniqInPlace(t *testing.T) {
+	in := []int{1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 7}
+	in = slices.UniqInPlace(in)
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7}, in)
+	assert.Equal(t, []int{}, slices.UniqInPlace([]int{}))
+}
