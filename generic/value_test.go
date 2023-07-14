e4aba95ace8ac00d746d0fa6af0f9081624e0639
Sterling Hanenkamp <sterling@speakeasyapi.dev>
2023-07-13 23:46:29 -0500

    fix incomplete coverage

--- generic/value_test.go
+++ generic/value_test.go
@@ -12,6 +12,7 @@ func TestFirstNonZero(t *testing.T) {
 	assert.Equal(t, 1, generic.FirstNonZero[int](0, 1, 2))
 	assert.Equal(t, 1, generic.FirstNonZero[int](1, 0, 2))
 	assert.Equal(t, 1, generic.FirstNonZero[int](0, 0, 1))
+	assert.Equal(t, 0, generic.FirstNonZero[int](0, 0, 0))
 }
 
 func TestFirstNonNil(t *testing.T) {
@@ -24,6 +25,7 @@ func TestFirstNonNil(t *testing.T) {
 	assert.Equal(t, *b, *generic.FirstNonNil(a, b, c))
 	assert.Equal(t, *b, *generic.FirstNonNil(b, a, c))
 	assert.Equal(t, *b, *generic.FirstNonNil(a, a, b))
+	assert.Nil(t, generic.FirstNonNil[int](nil, nil, nil))
 }
 
 func TestZero(t *testing.T) {
